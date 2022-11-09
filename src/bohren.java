a07cad048d6eefd750f0ae6652bd82d57008b3b6
huetvinc <huetvinc@kkg-do.eu>
2022-11-09 15:04:26 +0100

    Initial commit

---
+++ src/bohren.java
@@ -0,0 +1,22 @@
+import javax.xml.namespace.QName;
+
+public class bohren {
+
+    private int wat;
+    private String name;
+    private int age;
+    private double price;
+    private boolean broken;
+
+    public bohren(){
+        this.name= name;
+        this.wat= 1000;
+        this.age= 1;
+        this.price= 50;
+        this.broken= false;
+        
+
+
+    }
+
+}
